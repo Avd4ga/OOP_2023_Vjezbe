8c930e221fe023ad815227c8e2552e1298476e5d
Avd4ga <avdo.vladavic@stu.ibu.edu.ba>
2023-10-22 15:05:30 +0200

    Lab Week 3 - DB commit

--- src/main/java/LabWeek2/Lab2_2.java
+++ src/main/java/LabWeek2/Lab2_2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Lab2_2 {
     public static void main(String[] args) {
 
+
         Scanner scan=new Scanner(System.in);
 
         int sum=0;
@@ -18,3 +19,7 @@ public class Lab2_2 {
 
     }
 }
+
+
+
+
